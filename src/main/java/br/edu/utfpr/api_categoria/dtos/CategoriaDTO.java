ae1f828a61025e6218b52d48625b4500814760dc
Evandro Kuszera <evandrokuszera@gmail.com>
2024-11-12 21:44:52 -0300

    Adicionando validação ao CategoriaDTO

--- src/main/java/br/edu/utfpr/api_categoria/dtos/CategoriaDTO.java
+++ src/main/java/br/edu/utfpr/api_categoria/dtos/CategoriaDTO.java
@@ -1,3 +1,17 @@
 package br.edu.utfpr.api_categoria.dtos;
 
-public record CategoriaDTO(Long id, String name, Boolean active) {}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record CategoriaDTO(
+        Long id,
+
+        @NotNull(message = "Name é obrigatório")
+        @NotBlank(message = "Name é obrigatório")
+        @Size(min=3, max=50, message = "Name deve ter entre 3 e 50 caracteres")
+        String name,
+
+        @NotNull(message = "Active é obrigatório")
+        Boolean active
+) {}
